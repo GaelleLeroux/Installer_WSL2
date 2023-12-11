96ff7da25d0cc0eb73d118aa1b5af1a5bfe46764
GaelleLeroux <gaelleleroux974@gmail.com>
2023-12-11 08:55:41 -0500

    ENH : add new lib to install

---
+++ principal.py
@@ -0,0 +1,135 @@
+import ctypes
+import sys
+import os
+import time
+import subprocess
+import urllib.request
+import tempfile
+from colorama import init, Fore
+
+# Initialise colorama
+init(autoreset=True)
+
+# Check if the file is launch with admin right
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+# Write with a color in the terminal
+def print_instruction(message):
+    print(Fore.RED + message)
+    
+def print_command(message):
+    print(Fore.GREEN + message)
+
+# Check if ubuntu is installed on the computer
+def is_ubuntu_installed():
+    "Check if unbuntu install"
+    result = subprocess.run(['wsl', '--list'], capture_output=True, text=True)
+    output = result.stdout.encode('utf-16-le').decode('utf-8')
+    clean_output = output.replace('\x00', '')  # Enlève tous les octets null
+    return 'Ubuntu' in clean_output
+
+def ask_for_restart():
+    confirm = input("Do you want to restart now your computer (Yes/No) ? ")
+    if confirm.lower() in ['yes', 'y', '']:
+        # Restart the computer after 10s
+        subprocess.run(["shutdown", "/r", "/t", "10"])
+        print_command("Your computer will restart in 10s.")
+    else:
+        print_command("The restart is cancelled.")
+
+
+def main():
+    text = "When an interaction is requested, it is indicated in red. Press enter to continue."
+    print_instruction(text)
+    
+    input("Press enter")
+    
+    text = "The executable needs admin rights, press enter to give them."
+    print_instruction(text)
+    input("Press enter to give admin right")
+
+    exe_dir = os.path.dirname(sys.executable)
+    exe_name = "secondaire.exe"
+    exe_path = os.path.join(exe_dir,"utils", exe_name)
+    temp_file_path = os.path.join(exe_dir, "utils","tempo.txt")
+
+    with open(temp_file_path, 'a') as file:  # Ouverture du fichier en mode 'append'
+        file.write("Start of the tempo file" + '\n')  # Écrire le message suivi d'une nouvelle ligne
+
+    # L'argument que vous voulez passer à l'exécutable
+    kernel_path = os.path.join(os.path.expanduser("~"),"Downloads", "wsl_update_x64.msi")
+
+    if is_admin():
+        # Launch with subprocess secondaire if already in admin
+        subprocess.run([exe_path, kernel_path,temp_file_path])
+    else:
+        # Ask and launch secondaire.exe if not in admin
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", exe_path, f"{kernel_path} {temp_file_path}", None, 1)
+    
+    # Attendre que l'utilisateur ferme manuellement le terminal admin ou que le script se termine
+    
+    # Read the tempo.txt file
+    text = "Wait for a new instruction"
+    print_instruction(text)
+    with open(temp_file_path, 'r') as file:
+        output = file.read()
+        while "LetsContinue" not in output :
+            time.sleep(5)
+            output = file.read()
+            
+    # Delete tempo file
+    os.unlink(temp_file_path)
+            
+    if "restart" in output:
+        text = "Restart your computer and run this executable again."
+        print_instruction(text)
+        ask_for_restart()
+        input("Wait for it to restart, or press enter to quit if restart is cancelled.")
+        
+    if "errorKernel" in output:
+        text = "An error occurred during installation of the linux kernel. Have you restarted the computer?"
+        print_instruction(text)
+        ask_for_restart()
+        input("Wait for it to restart, or press enter to quit if restart is cancelled.")
+        
+    if "ready" in output:
+        if not is_ubuntu_installed(): # check is ubuntu is install on wsl
+                #define version of wsl at 2
+                subprocess.run("wsl --set-default-version 2", shell=True)
+
+                # Write a message for the user before installing
+                text = "A new windows will open!\nYou will create an username with a password, please note that whilst entering the Password, nothing will appear on screen. This is called blind typing. You won't see what you are typing, this is completely normal.\nWhen the line starts with your username, please enter 'exit' to continue the installation on this window."
+                print_instruction(text)
+                input("Press enter to continue")
+                # input("During the installation you will create an username with a password, please note that whilst entering the Password, nothing will appear on screen. This is called blind typing. You won't see what you are typing, this is completely normal.\nWhen the line will start by your username please enter 'exit' to continue the installation\nPress enter to start the installation, it may take a few minutes")
+                subprocess.check_call(["wsl", "--install","-d","Ubuntu"])
+                
+                # Wait for Ubuntu to be installed on WSL
+                while not is_ubuntu_installed():
+                    time.sleep(10)
+                    
+        text = "Enter the password you created when installing wsl" 
+        print_instruction(text)
+        input("Press enter to write the password")
+        exe_dir = os.path.dirname(sys.executable)
+            
+        bash_script = "install_libs.sh"
+
+        full_bash_script_path = os.path.join(exe_dir,"utils", bash_script)
+
+        # Path to the .sh with the command to install librairie on wsl
+        wsl_path = "/mnt/" + full_bash_script_path[0].lower() + full_bash_script_path[2:].replace("\\", "/")
+            
+        # Once WSL is initialized, execute the bash script (to install the librairies)
+        subprocess.run(["wsl", "bash","-c", wsl_path])
+        
+        print_instruction("Installation succesfull")
+        input("Press enter to quit")
+        
+    
+if __name__ == "__main__":
+    main()
---
+++ secondaire.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-cd D
+import ctypes
+import sys
+import os
+import time
+import subprocess
+import urllib.request
+import re
+import argparse
+from colorama import init, Fore
+
+# Initialiser colorama
+init(autoreset=True)
+
+# Check if ubuntu is installed on the computer
+def is_ubuntu_installed():
+    result = subprocess.run(['wsl', '--list'], capture_output=True, text=True)
+    output = result.stdout.encode('utf-16-le').decode('utf-8')
+    clean_output = output.replace('\x00', '')  # Enlève tous les octets null
+    return 'Ubuntu' in clean_output
+
+# check if the code is running in administrator
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+def download_wsl2_kernel(kernel_path):
+    kernel_url = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+    urllib.request.urlretrieve(kernel_url, kernel_path)
+    return kernel_path 
+
+# Install the kernel of linux
+def install_wsl2_kernel(kernel_path):
+    subprocess.run(["msiexec", "/i", kernel_path, "/quiet"], check=True)
+
+def check_wsl2_kernel_installed():
+    try:
+        # Run the WSL status command to get information about the WSL installations
+        result = subprocess.run("wsl --status", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        
+        # If the command was successful, process the output
+        if result.returncode == 0:
+            # output = result.stdout
+            output_nclean = result.stdout.encode('utf-16-le').decode('utf-8')
+            output = output_nclean.replace('\x00', '')  # Enlève tous les octets null
+            
+            # Look for a line that indicates the WSL version
+            kernel_version_search = re.search(r'Kernel version: (\d+\.\d+\.\d+)', output)
+            
+            if kernel_version_search:
+                kernel_version = kernel_version_search.group(1)
+                return True
+            else:
+                return False
+        else:
+            return None
+        
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        print(f"Error output: {e.stderr}")
+        return None
+    
+def print_command(message):
+    '''
+    print in green in the terminal
+    '''
+    print(Fore.GREEN + message)
+
+def write_output(file_path,message):
+    '''
+    write in the temprary file
+    '''
+    with open(file_path, 'a') as file:  # Ouverture du fichier en mode 'append'
+        file.write(message + '\n')  # Écrire le message suivi d'une nouvelle ligne
+        
+def main(kernel_path,file_path):
+
+    print_command("This windows will be close automatically, please wait")
+
+    # Check is WSL activate in parameters of windows
+    wsl_status = subprocess.run("dism.exe /online /get-featureinfo /featurename:Microsoft-Windows-Subsystem-Linux",
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+    
+    virtual_machine_status = subprocess.run("dism.exe /online /get-featureinfo /featurename:VirtualMachinePlatform",
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+
+    
+    activate_wsl = False
+    if "Enabled" not in wsl_status.stdout:
+            # Activate WSL and virtual machine platform for wsl2
+            subprocess.run("dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart", shell=True)
+            # need to restart the computer
+            activate_wsl = True
+            write_output(file_path,"restart")
+
+    if "Enabled" not in virtual_machine_status.stdout:
+            subprocess.run("dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart", shell=True)
+            activate_wsl = True
+            write_output(file_path,"restart")
+       
+    test_kernel=False
+    if not activate_wsl :     
+        test_kernel = check_wsl2_kernel_installed()
+        if test_kernel!=True:
+            try : 
+                kernel_path = download_wsl2_kernel(kernel_path)
+                install_wsl2_kernel(kernel_path)
+                test_kernel=True
+            except:
+                write_output(file_path,"errorKernel")
+        
+    if test_kernel==True:
+        write_output(file_path,"ready")
+    write_output(file_path,"LetsContinue")
+        
+        
+        
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2])
